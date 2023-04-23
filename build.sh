0ace950f45845549ca1a3ffa879826408e6ff58d
Sahil Panchasara <sahil.panchasara@techdome.net.in>
2023-04-23 21:48:32 +0530

    updated

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PROTOC_VERSION=3.19.0
+PROTOC_URL=https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOC_VERSION}/protoc-${PROTOC_VERSION}-linux-x86_64.zip
+
+echo "Installing protobuf compiler version $PROTOC_VERSION..."
+curl -OL $PROTOC_URL
+unzip protoc-${PROTOC_VERSION}-linux-x86_64.zip -d /usr/local
